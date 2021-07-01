a4a47a274cd530b5365e3eb790e63a988bdf73e3
bczhang <445767454@qq.com>
2021-07-01 13:46:17 +0800

    abc

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 		utils.Log.WithField("err", err).Error("start error")
 		return
 	}
-	err = c.AddFunc("0 */49 * * * *", func() {
+	err = c.AddFunc("0 */5 * * * *", func() {
 		controllers.RemoveSession()
 		controllers.UploadWebLock()
 	})
